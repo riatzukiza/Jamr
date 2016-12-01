a7ddbad1b2c94c446afaa44ab22d8cb04b52e221
Your Name <you@example.com>
2016-12-01 15:24:11 -0600

    WIP on master: 6c456dc initial commit

--- run.js
+++ run.js
@@ -2,10 +2,10 @@ var program = require("commander");
 
 program
     .version("0.0.1")
-    .option("-s, --output-size [number]", "number of terms generated from language model")
+    .option("-s, --output-size [number]", "number of terms generated from language model", 5)
     .option("-i, --input [path]", "path of training data")
     .option("-o, --output-dest [path]", "location to send output, defaults to stdout")
-    .option("-n, --context-size [number]","the 'n' in n-gram. How deep does the model go?")
+    .option("-n, --context-size [number]","the 'n' in n-gram. How deep does the model go?",2)
     .parse(process.argv);
 
 global.curry = require("./lib/functional/partialApplication.js").curry
@@ -19,12 +19,17 @@ const {
     chainPromise
 } = require("./lib/promise")
 
+var {input,outputSize,outputDest,contextSize} = program;
+outputSize = parseInt(outputSize);
+contextSize = parseInt(contextSize);
+
+console.log("running program with inputs",{input,outputSize,outputDest,contextSize})
 chainPromise(null, [
-        () => testModel.buildFromPath(program.input),
-        () => testModel.randomWalk(program.outputSize),
+        () => testModel.buildFromPath(input),
+        () => testModel.randomWalk(outputSize),
         (output) => {
-            if (program.outputDest)
-                fs.writeFile(program.outputDest, output, (err) => {
+            if (outputDest)
+                fs.writeFile(outputDest, output, (err) => {
                     if (err) {
                         throw err;
                     }
